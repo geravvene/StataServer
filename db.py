da8fe01a1278abb6ca65cbe1115b35cdb0c72bc3
geravvene <romanov17903@gmail.com>
2024-08-20 07:50:20 +0300

    test

--- db.py
+++ db.py
@@ -11,38 +11,12 @@ def add(data, name):
     db = client['StataBot']
     collection = db[name]
     
-    last_order_date=collection.find().sort({'$natural': -1}).limit(1)[0]["DATE"]
     orders=[]
 
     for row in data:
         row["DATE"]=datetime.strptime(row["DATE"], '%Y.%m.%d %H:%M:%S')
-        if row["DATE"] > last_order_date:
-            orders.append(row)
-        else: 
-            break
-        
-    sub='to #'
-    removes=[]
-    for row in orders:
-        print(row)
-        if sub in row['COMMENT']:
-            id=int(row['COMMENT'].replace(sub,''))
-            index=find_by_key(orders, '_id', id)
-            if index:
-                row['PROFIT']+=orders[index]['PROFIT']
-                row['SWAP']+=orders[index]['SWAP']
-                row['COMMISSION']+=orders[index]['COMMISSION']
-                row['TO']=orders[index]['COMMISSION']
-                if sub in orders[index]['COMMENT']:
-                    row['TO']=int(orders[index]['COMMISSION'].replace(sub,''))
-                    
-                removes.append(orders[index])
-            else:
-                row['TO']=id
-   
-    for row in removes:
-        orders.remove(row)
-        
+        orders.append(row)
+
     orders.reverse()
     
     collection.insert_many(orders)   
